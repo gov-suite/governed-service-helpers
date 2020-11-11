4346cb2765ff80c35af87b253e01b1d95babef18
Shahid N. Shah <shahid@shah.org>
2020-11-11 18:17:10 -0500

    feat: add typical middleware

--- oak.ts
+++ oak.ts
@@ -1,3 +1,4 @@
+import * as colors from "https://deno.land/std@0.77.0/fmt/colors.ts";
 import * as oak from "https://deno.land/x/oak@v6.3.2/mod.ts";
 import * as health from "./health.ts";
 
@@ -25,4 +26,60 @@ export function registerHealthRoute(
       ctx.response.body = supplier.serviceVersion;
     });
   app.use(router.routes());
+}
+
+export const responseTimeHeaderName = "X-Response-Time";
+
+export interface AccessReport {
+  readonly responseTime: number;
+}
+
+export interface AccessReporter {
+  (ctx: oak.Context<Record<string, unknown>>, report: AccessReport): void;
+}
+
+export function defaultAccessReporter(
+  ctx: oak.Context<Record<string, unknown>>,
+  report: AccessReport,
+): void {
+  console.log(
+    `${colors.green(ctx.request.method)} ${
+      colors.brightBlue(ctx.response.status.toString())
+    } ${colors.yellow(ctx.request.url.toString())} - ${
+      colors.gray(report.responseTime.toString())
+    }`,
+  );
+}
+
+export interface TypicalMiddlewareOptions {
+  readonly accessReporter?: AccessReporter;
+}
+
+export function registerTypicalMiddleware(
+  app: oak.Application,
+  options?: {
+    accessReporter?: AccessReporter;
+  },
+): void {
+  if (options?.accessReporter) {
+    const reporter = options?.accessReporter;
+    app.use(async (ctx, next) => {
+      await next();
+      reporter(
+        ctx,
+        {
+          responseTime: Number.parseInt(
+            ctx.response.headers.get(responseTimeHeaderName) || "-1",
+          ),
+        },
+      );
+    });
+  }
+
+  app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const responseTime = Date.now() - start;
+    ctx.response.headers.set(responseTimeHeaderName, `${responseTime}`);
+  });
 }
