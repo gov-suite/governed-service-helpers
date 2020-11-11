cb13d6e624db1c7e109aca3cd014ef3fd5606e4f
Shahid N. Shah <shahid@shah.org>
2020-11-11 15:29:43 -0500

    fix: add for file URLs

--- version.ts
+++ version.ts
@@ -1,7 +1,7 @@
 import { fs, path, shell } from "./deps.ts";
 
 export interface ModuleVersionSupplier<O> {
-  (importMetaURL: string, options?: O): Promise<string>;
+  (importMetaURL: URL | string, options?: O): Promise<string>;
 }
 
 export interface DetermineVersionFromRepoTagOptions {
@@ -13,29 +13,34 @@ export interface DetermineVersionFromRepoTagOptions {
 }
 
 export async function determineVersionFromRepoTag(
-  importMetaURL: string,
+  importMetaURL: URL | string,
   options?: DetermineVersionFromRepoTagOptions,
 ): Promise<string> {
   // if we're running locally, see if Git tag can be discovered
-  if (fs.existsSync(path.fromFileUrl(importMetaURL))) {
-    let version = "v0.0.0";
-    await shell.runShellCommand(
-      "git describe --tags --abbrev=0",
-      {
-        onCmdComplete: (execResult) => {
-          if (execResult.code == 0) {
-            version = new TextDecoder().decode(execResult.stdOut).trim();
-          } else {
-            if (options?.onInvalidLocalVersion) {
-              version = options.onInvalidLocalVersion(execResult);
+  const url = importMetaURL instanceof URL
+    ? importMetaURL
+    : new URL(importMetaURL);
+  if (url.protocol == "file:") {
+    if (fs.existsSync(path.fromFileUrl(importMetaURL))) {
+      let version = "v0.0.0";
+      await shell.runShellCommand(
+        "git describe --tags --abbrev=0",
+        {
+          onCmdComplete: (execResult) => {
+            if (execResult.code == 0) {
+              version = new TextDecoder().decode(execResult.stdOut).trim();
             } else {
-              version = `v?.?.${execResult.code}`;
+              if (options?.onInvalidLocalVersion) {
+                version = options.onInvalidLocalVersion(execResult);
+              } else {
+                version = `v?.?.${execResult.code}`;
+              }
             }
-          }
+          },
         },
-      },
-    );
-    return `${version}-local`;
+      );
+      return `${version}-local`;
+    }
   }
 
   // if we're running remote, get the version from the URL in the format
@@ -45,12 +50,12 @@ export async function determineVersionFromRepoTag(
       `${options.repoIdentity}\/v?(?<version>\d+\.\d+\.\d+)\/`,
     )
     : /\/v?(?<version>\d+\.\d+\.\d+)\//;
-  const matched = importMetaURL.match(repoVersionRegExp);
+  const matched = url.href.match(repoVersionRegExp);
   if (matched) {
     return `v${matched.groups!["version"]}`;
   }
   if (options?.onInvalidRemoteMatch) {
     return options.onInvalidRemoteMatch(repoVersionRegExp);
   }
-  return `v0.0.0-remote(no match for ${repoVersionRegExp} in '${importMetaURL}')`;
+  return `v0.0.0-remote(no match for ${repoVersionRegExp} in '${url}')`;
 }
