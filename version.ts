933217f9027aafb2caedeea10e179ac36c31ab3e
Shahid N. Shah <shahid@shah.org>
2020-11-19 15:34:04 -0500

    feat: trap exception if run without git

--- version.ts
+++ version.ts
@@ -1,6 +1,6 @@
 import * as fs from "https://deno.land/std@0.77.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.77.0/path/mod.ts";
-import * as shell from "https://denopkg.com/shah/tsd-shell@v0.9.1/mod.ts";
+import * as shell from "https://denopkg.com/shah/tsd-shell@v0.9.2/mod.ts";
 
 export interface ModuleVersionSupplier<O> {
   (importMetaURL: URL | string, options?: O): Promise<string>;
@@ -8,8 +8,12 @@ export interface ModuleVersionSupplier<O> {
 
 export interface DetermineVersionFromRepoTagOptions {
   readonly repoIdentity?: string;
+  readonly defaultIfIndeterminate?: string;
   readonly onInvalidLocalVersion?: (
     execResult: shell.RunShellCommandExecResult,
+  ) => string;
+  readonly onGitDescribeError?: (
+    scer: shell.ShellCommandExceptionResult,
   ) => string;
   readonly onInvalidRemoteMatch?: (repoVersionRegExp: RegExp) => string;
 }
@@ -25,9 +29,20 @@ export async function determineVersionFromRepoTag(
   if (url.protocol == "file:") {
     if (fs.existsSync(path.fromFileUrl(importMetaURL))) {
       let version = "v0.0.0";
-      await shell.runShellCommand(
+      await shell.runShellCommandSafely(
         "git describe --tags --abbrev=0",
         {
+          onException: (scer: shell.ShellCommandExceptionResult) => {
+            if (options?.onGitDescribeError) {
+              version = options?.onGitDescribeError(scer);
+            } else {
+              console.log(
+                `Error running 'git describe' in ${Deno.cwd()}: ${scer.error}`,
+              );
+              version = options?.defaultIfIndeterminate || "v?.?.?";
+            }
+            return scer;
+          },
           onCmdComplete: (execResult) => {
             if (execResult.code == 0) {
               version = new TextDecoder().decode(execResult.stdOut).trim();
@@ -35,7 +50,8 @@ export async function determineVersionFromRepoTag(
               if (options?.onInvalidLocalVersion) {
                 version = options.onInvalidLocalVersion(execResult);
               } else {
-                version = `v?.?.${execResult.code}`;
+                version = options?.defaultIfIndeterminate ||
+                  `v?.?.${execResult.code}`;
               }
             }
           },
